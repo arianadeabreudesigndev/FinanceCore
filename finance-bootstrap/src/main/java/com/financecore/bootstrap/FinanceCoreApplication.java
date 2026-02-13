005bebdd9a3b5d4e8a7fb34440bdbcfe430ae055
Ariana de Abreu <arianadeabreudesigndev@gmail.com>
2026-02-13 06:48:56 -0300

    chore(bootstrap): adiciona classe principal Spring Boot

---
+++ finance-bootstrap/src/main/java/com/financecore/bootstrap/FinanceCoreApplication.java
@@ -0,0 +1,11 @@
+package com.financecore.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceCoreApplication.class, args);
+    }
+}
